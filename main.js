4612c308b05625f6f48b204d1a258e20ee92af76
pablo806 <pablo.gonzalez0m@gmail.com>
2025-12-12 17:40:36 +0100

    1212

--- main.js
+++ main.js
@@ -1,28 +1,4 @@
-const buttons = document.querySelectorAll(".btn");
-buttons.forEach(button => {
-    const text = button.querySelector(".bug-text");
-    const originalText= text.innerText;
-    const delay = 30;
 
-    button.addEventListener("mouseover", () => {
-        let i = 0;
-        const interval = setInterval(() => {
-            const modifiedText = originalText
-                .split("")
-                .map(() => {
-                return String.fromCharCode(Math.random() * (126-32)+32);
-            
-                })
-                .join("");
-            text.innerText = modifiedText;
-            i++;
-            if (i>7){
-                clearInterval(interval);
-                text.innerText = originalText;
-            }
-        }, delay);
-    });
-});
 
 document.querySelectorAll(".list .image img").forEach(image => {
     image.onclick = () => {
