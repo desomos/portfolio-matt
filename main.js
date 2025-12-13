3a7001f21e9b1b9e88996f3943975960395286f2
pablo806 <pablo.gonzalez0m@gmail.com>
2025-12-13 14:41:24 +0100

    qazedqs

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
 const popup = document.querySelector('.popup-image');
 const popupImg = popup.querySelector('img');
-const popupVideo = popup.querySelector('video');
 
-// Click sur une image ou une vidéo
 document.querySelectorAll('.image img').forEach(img => {
     img.addEventListener('click', () => {
         popup.style.display = 'block';
-
-        // IMAGE
-        if (img.dataset.full) {
-            popupVideo.pause();
-            popupVideo.style.display = 'none';
-            popupVideo.src = '';
-
-            popupImg.style.display = 'block';
-            popupImg.src = img.dataset.full;
-        }
-
-        // VIDEO
-        if (img.dataset.video) {
-            popupImg.style.display = 'none';
-            popupImg.src = '';
-
-            popupVideo.style.display = 'block';
-            popupVideo.src = img.dataset.video;
-            popupVideo.play();
-        }
+        popupImg.src = img.dataset.full;
     });
 });
 
-// Fermer la popup
 popup.addEventListener('click', () => {
     popup.style.display = 'none';
-
     popupImg.src = '';
-    popupVideo.pause();
-    popupVideo.src = '';
 });
