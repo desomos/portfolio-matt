8d521f41dc23910850b4f69640d8b936bc3b5da3
pablo806 <pablo.gonzalez0m@gmail.com>
2025-12-13 14:07:58 +0100

    dsqzeaqsd

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 const popup = document.querySelector('.popup-image');
 const popupImg = popup.querySelector('img');
+const popupVideo = popup.querySelector('video');
 
+// Click sur une image ou une vidéo
 document.querySelectorAll('.image img').forEach(img => {
     img.addEventListener('click', () => {
         popup.style.display = 'block';
-        popupImg.src = img.dataset.full;
+
+        // IMAGE
+        if (img.dataset.full) {
+            popupVideo.pause();
+            popupVideo.style.display = 'none';
+            popupVideo.src = '';
+
+            popupImg.style.display = 'block';
+            popupImg.src = img.dataset.full;
+        }
+
+        // VIDEO
+        if (img.dataset.video) {
+            popupImg.style.display = 'none';
+            popupImg.src = '';
+
+            popupVideo.style.display = 'block';
+            popupVideo.src = img.dataset.video;
+            popupVideo.play();
+        }
     });
 });
 
+// Fermer la popup
 popup.addEventListener('click', () => {
     popup.style.display = 'none';
+
     popupImg.src = '';
+    popupVideo.pause();
+    popupVideo.src = '';
 });
